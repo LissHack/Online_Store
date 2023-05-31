1f9091f57c5b2f52544ac66300fbaa201c587e3a
Liss <les.karo@yandex.ru>
2023-06-01 00:22:16 +0400

    CRUD для типов

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const PORT = process.env.PORT || 6000
 const router = require('./routers/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 // Обработка ошибок, последний Middleware
