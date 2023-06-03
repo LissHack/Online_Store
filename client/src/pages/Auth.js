2bac99918e64c13ec0d3ea481812a6a4c799d48b
Liss <les.karo@yandex.ru>
2023-06-03 18:02:30 +0400

    Авторизация, регистрация

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {Button, Card, Container, Form, Row} from "react-bootstrap";
-import {NavLink} from "react-router-dom";
-import {REGISTRATION_ROUTE} from "../utils/consts";
+import {NavLink, useLocation} from "react-router-dom";
+import {LOGIN_ROUTE, REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
+    const location = useLocation()
+    const isLogin = location.pathname === LOGIN_ROUTE
+
     return (
         <Container className="d-flex justify-content-center align-items-center"
                    style={{height: window.innerHeight - 54}}
         >
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">Авторизация</h2>
+                <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control
                         className="mt-4"
@@ -18,15 +21,17 @@ const Auth = () => {
                         className="mt-4"
                         placeholder="Введите ваш пароль"/>
                     <Row className="d-flex justify-content-between mt-4 pe-4 ps-2">
-                        <div>
-                            Нет аккаунта?
-                            <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
-                        </div>
+                        {isLogin ?
+                            <div>
+                                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+                            </div>
+                            : <div>
+                                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
+                            </div>
+                        }
                     </Row>
-                    <Button
-                        className="align-self-end"
-                        variant={"outline-success"}>
-                        Войти
+                    <Button className="align-self-end" variant={"outline-success"}>
+                        {isLogin ? 'Войти' : 'Регистрация'}
                     </Button>
                 </Form>
             </Card>
