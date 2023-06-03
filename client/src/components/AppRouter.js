7d83c5b3635de296adc677d4f57aa94158e2721f
Liss <les.karo@yandex.ru>
2023-06-03 14:04:16 +0400

    Mobx, глобальный стейт, UserStore

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import {Switch, Route, Redirect} from 'react-router-dom'
+import React, {useContext} from 'react';
+import {Routes, Route, Navigate} from 'react-router-dom'
+import {authRoutes, publicRoutes} from "../routes";
+import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 const AppRouter = () => {
 
+    const {user} = useContext(Context)
     return (
-
-        <Switch>
-            
-        </Switch>
+        <Routes>
+            {user.isAuth && authRoutes.map(({path, Component}) =>
+                <Route
+                    key={path}
+                    path={path}
+                    element={<Component/>} exact
+                />
+            )}
+            {publicRoutes.map(({path, Component}) =>
+                <Route
+                    key={path}
+                    path={path}
+                    element={<Component/>} exact
+                />
+            )}
+            <Route path='*' element={<Navigate to={SHOP_ROUTE}/>}/>
+        </Routes>
     );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import UserStore from "./store/UserStore";
+import DeviceStore from "./store/DeviceStore";
 
+export const Context = createContext(null)
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <Context.Provider value={{
+        user: new UserStore(),
+        device: new DeviceStore(),
+    }}>
+        <App/>
+    </Context.Provider>,
+
 )
