bd420ad6c9e366996e063be34de0e5a0a2b66f53
Liss <les.karo@yandex.ru>
2023-06-03 17:55:07 +0400

    Страница регистрации

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
+import {REGISTRATION_ROUTE} from "../utils/consts";
 
 const Auth = () => {
     return (
-        <div>
-            AUTH
-        </div>
-    );
+        <Container className="d-flex justify-content-center align-items-center"
+                   style={{height: window.innerHeight - 54}}
+        >
+            <Card style={{width: 600}} className="p-5">
+                <h2 className="m-auto">Авторизация</h2>
+                <Form className="d-flex flex-column">
+                    <Form.Control
+                        className="mt-4"
+                        placeholder="Введите ваш email"/>
+                    <Form.Control
+                        className="mt-4"
+                        placeholder="Введите ваш пароль"/>
+                    <Row className="d-flex justify-content-between mt-4 pe-4 ps-2">
+                        <div>
+                            Нет аккаунта?
+                            <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+                        </div>
+                    </Row>
+                    <Button
+                        className="align-self-end"
+                        variant={"outline-success"}>
+                        Войти
+                    </Button>
+                </Form>
+            </Card>
+        </Container>
+    )
+        ;
 };
 
 export default Auth;
