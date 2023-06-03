247ef0792be87b525795e9a3701ac65eee1ee97e
Liss <les.karo@yandex.ru>
2023-06-03 20:41:35 +0400

    создание бренда товара

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -11,6 +11,8 @@ export default class DeviceStore {
         this._brands = [
             {id: 1, name: 'Samsung'},
             {id: 2, name: 'Apple'},
+            {id: 3, name: 'Lenovo'},
+            {id: 4, name: 'Asus'},
         ]
         this._devices = [
             {id: 1, name: 'Iphone 12 pro', price: 25000, rating: 5},
@@ -19,6 +21,7 @@ export default class DeviceStore {
             {id: 4, name: 'Iphone 12 pro', price: 25000, rating: 5},
         ]
         this._selectedType = {}
+        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -34,9 +37,14 @@ export default class DeviceStore {
         this._devices = devices
     }
 
-    setSelectedType(type){
+    setSelectedType(type) {
         this._selectedType = type
     }
+
+    setSelectedBrand(brand) {
+        this._selectedBrand = brand
+    }
+
     get types() {
         return this._types
     }
@@ -51,6 +59,10 @@ export default class DeviceStore {
 
     get selectedType() {
         return this._selectedType
+    }
+
+    get selectedBrand() {
+        return this._selectedBrand
     }
 
 }
