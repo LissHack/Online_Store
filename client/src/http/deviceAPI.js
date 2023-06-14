4e33e24eda6da026cb79a47fa708c085446faedb
Liss <les.karo@yandex.ru>
2023-06-14 16:00:31 +0400

    Получение брендов (подгрузка данных о брендах из бд)

--- client/src/http/deviceAPI.js
+++ client/src/http/deviceAPI.js
@@ -6,7 +6,17 @@ export const createType = async (type) => {
     return data
 }
 
-export const fetchType = async () => {
-    const {data} = await $host.post('api/type')
+export const fetchTypes = async () => {
+    const {data} = await $host.get('api/type')
+    return data
+}
+
+export const createBrand = async (brand) => {
+    const {data} = await $authHost.post('api/brand',brand)
+    return data
+}
+
+export const fetchBrands = async () => {
+    const {data} = await $host.get('api/brand')
     return data
 }
